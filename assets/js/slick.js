a5984aad3ece44e38870bc8d6a76be3aa560a4b0
ikbalgunawan <ikbalgunawan2122@gmail.com>
2023-11-15 13:17:23 +0700

    msm

---
+++ assets/js/slick.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.partners-logos').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
